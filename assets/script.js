3c7b96bb2a2852fb1651444c91c32a5f63c3faa6
AlfredoJi <mralfredojimenez1996@gmail.com>
2023-02-28 18:53:01 -0500

    added code and comments

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,131 @@
-var saveButton = document.getElementsByTagName("button");
 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// All code in one JQuery function so the code doesnt run till all the elements in the html render.
 $(function () {
+  // Using dayjs to get the current time in a variable.
+  var currentHour = dayjs().hour();
+  // Using JQuery to get the ID "currentDay" and adding the current date using dayjs and putting it in the html.
+  $("#currentDay").text(dayjs().format("MMM DD YYYY"));
+  // Getting the data-hour that was set in the html.
+  var hourNine = $("#hour-9")[0].dataset.hour;
+  // Using a if statement to compare the current hour to that data-hour.
+  if (currentHour > hourNine) {
+    // If current hour is greater then data-hour remove "present" from the class and add "past".
+    $("#hour-9").removeClass("present").addClass("past");
+  } else if (currentHour < hourNine) {
+    // If current hour is less then data-hour remove "present" from the class and add "future".
+    $("#hour-9").removeClass("present").addClass("future");
+  } else {
+    // Else keep the "present" class.
+    $("#hour-9").removeClass("present").addClass("present");
+  }
 
-  $(saveButton).click(function () {
-    console.log("work");
-    var timeBlock = this.getElementsByTagName("div");
-    var textArea = this.getElementsByTagName("textarea");
-    localStorage.setItem(timeBlock, "textArea");
-    localStorage.getItem(timeBlock);
+  // This is just getting the textarea from local storage if there was anything there when the page loads.
+  $("#hour-9 textarea").val(localStorage.getItem("9"));
 
+  var hourTen = $("#hour-10")[0].dataset.hour;
 
+  if (currentHour > hourTen) {
+    $("#hour-10").removeClass("present").addClass("past");
+  } else if (currentHour < hourTen) {
+    $("#hour-10").removeClass("present").addClass("future");
+  } else {
+    $("#hour-10").removeClass("present").addClass("present");
+  }
+
+  $("#hour-10 textarea").val(localStorage.getItem("10"));
+
+  var hourEleven = $("#hour-11")[0].dataset.hour;
+
+  if (currentHour > hourEleven) {
+    $("#hour-11").removeClass("present").addClass("past");
+  } else if (currentHour < hourEleven) {
+    $("#hour-11").removeClass("present").addClass("future");
+  } else {
+    $("#hour-11").removeClass("present").addClass("present");
+  }
+
+  $("#hour-11 textarea").val(localStorage.getItem("11"));
+
+  var hourTwelve = $("#hour-12")[0].dataset.hour;
+
+  if (currentHour > hourTwelve) {
+    $("#hour-12").removeClass("present").addClass("past");
+  } else if (currentHour < hourNine) {
+    $("#hour-12").removeClass("present").addClass("future");
+  } else {
+    $("#hour-12").removeClass("present").addClass("present");
+  }
+
+  $("#hour-12 textarea").val(localStorage.getItem("12"));
+
+  var hourOne = $("#hour-1")[0].dataset.hour;
+
+  if (currentHour > hourOne) {
+    $("#hour-1").removeClass("present").addClass("past");
+  } else if (currentHour < hourOne) {
+    $("#hour-1").removeClass("present").addClass("future");
+  } else {
+    $("#hour-1").removeClass("present").addClass("present");
+  }
+
+  $("#hour-1 textarea").val(localStorage.getItem("13"));
+
+  var hourTwo = $("#hour-2")[0].dataset.hour;
+
+  if (currentHour > hourTwo) {
+    $("#hour-2").removeClass("present").addClass("past");
+  } else if (currentHour < hourTwo) {
+    $("#hour-2").removeClass("present").addClass("future");
+  } else {
+    $("#hour-2").removeClass("present").addClass("present");
+  }
+
+  $("#hour-2 textarea").val(localStorage.getItem("14"));
+
+  var hourThree = $("#hour-3")[0].dataset.hour;
+
+  if (currentHour > hourThree) {
+    $("#hour-3").removeClass("present").addClass("past");
+  } else if (currentHour < hourThree) {
+    $("#hour-3").removeClass("present").addClass("future");
+  } else {
+    $("#hour-3").removeClass("present").addClass("present");
+  }
+
+  $("#hour-3 textarea").val(localStorage.getItem("15"));
+
+  var hourFour = $("#hour-4")[0].dataset.hour;
+
+  if (currentHour > hourFour) {
+    $("#hour-4").removeClass("present").addClass("past");
+  } else if (currentHour < hourFour) {
+    $("#hour-4").removeClass("present").addClass("future");
+  } else {
+    $("#hour-4").removeClass("present").addClass("present");
+  }
+
+  $("#hour-4 textarea").val(localStorage.getItem("16"));
+
+  var hourFive = $("#hour-5")[0].dataset.hour;
+
+  if (currentHour > hourFive) {
+    $("#hour-5").removeClass("present").addClass("past");
+  } else if (currentHour < hourFive) {
+    $("#hour-5").removeClass("present").addClass("future");
+  } else {
+    $("#hour-5").removeClass("present").addClass("present");
+  }
+
+  $("#hour-5 textarea").val(localStorage.getItem("17"));
+
+  // This is a button event listener for the save button.
+  $("button").click(function () {
+    // Grabbing the text from textarea of the timeblock that was saved.
+    var textBox = $(this).siblings("textarea").val();
+    // Grabbing the data-hour of the said timeblock that was pressed.
+    var timeBlock = $(this).parent()[0].dataset.hour;
+    // Saving the timeblock and text area to local storage so they can be grabbed if needed.
+    localStorage.setItem(timeBlock, textBox);
   })
-
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+ 
 });
